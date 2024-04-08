5aef9bd0e7aa6785c8f72bcf2c73596ec561b383
bnchk <108313235+bnchk@users.noreply.github.com>
2024-04-08 18:52:56 +1000

    v0.2 - secrets file, install folder, misc
    - changed install folder from /opt/my_scripts/ to /opt/my_scripts/autobox/
    - remove keys into secrets file in run folder + parse these secrets
    - remove pre reboot warning message as has had 100% ok for long enough to assume it wont hang for unknown reasons
    - clearer message headings like AUTOBOX OK etc

--- autobox/autobox.sh
+++ autobox/autobox.sh
@@ -1,20 +1,37 @@
 #!/bin/bash
-#----------
+#==========
 # AUTOBOX - system self maintainer - for systemd to call (or cron)
-#----------
+#==========
 
-# Secrets to hide
-apitoken="azzzzzzzzzzzzzzzzzzzzzzzzzzzzz"   #monitoring
-usrtoken="uzzzzzzzzzzzzzzzzzzzzzzzzzzzzz"   #user
-service=""
+#=======================
+# CONFIGURATION REQUIRED
+#=======================
+# - Copy this script in:
+#      sudo mkdir -p /opt/my_scripts/autobox
+#      sudo touch /opt/my_scripts/autobox/autobox.sh
+#      sudo chmod 700 /opt/my_scripts/autobox/autobox.sh
+#        and copy this into it..
+# - Create secrets file referenced in user variable below
+#      sudo touch /opt/my_scripts/autobox/secrets.txt
+#      sudo chmod 700 /opt/my_scripts/autobox/secrets.txt
+#      sudo vi /opt/my_scripts/autobox/secrets.txt
+#        usrtoken="userkeyuserkeyuserkeyuserkeyzz"   #user
+#        apitoken="apikeyapikeyapikeyapikeyapikey"   #application key
+#        service="whateverit.service"                #optional service that is to be stopped
+# - To reboot without DOS style pop-up prompts stopping process needs config changed:
+#     sudo vim /etc/needrestart/needrestart.conf
+#      - Outdated Daemons warning (UNcomment + change i->a):
+#         - #$nrconf{restart} = 'i';  ==>  
+#         - $nrconf{restart} = 'a';
+#      - Kernel updates notification - UNcomment this line:   
+#         - $nrconf{kernelhints} = -1;
+# - To run sudo without password storage or promptingm add whole script to sudoers so everything within it is run with sudo
+#      use command:  sudo visudo  # and add line at end:
+#                    dog ALL=(ALL) NOPASSWD: /opt/my_scripts/autobox/autobox.sh
+# - schedule once a day, run at 8:30am for now, and at reboot:  
+#          crontab -e //  30 8 * * * sudo /opt/my_scripts/autobox/autobox.sh
+#                         @reboot sudo /opt/my_scripts/autobox/autobox.sh
 
-#========
-# HISTORY
-#========
-# v0.1a - beta testig triggers
-# v0.1b - pilot running iagon (with service) maintaining box ok
-# v0.1  - no service capability - was ok, just added message into HELP when cant stop service
-#       - check if running already and message + exit if so
 
 #======
 # NOTES
@@ -22,27 +39,19 @@ service=""
 # - Security updates are automatically via unattendend-upgrades package once a day at random time, usually without needing a reboot.
 # - But when it does need a reboot, this script looks after it.
 
-#=======================
-# CONFIGURATION REQUIRED
-#=======================
-# - Copy this script in:
-#      sudo mkdir -p /opt/my_scripts
-#      sudo touch /opt/my_scripts/autobox.sh
-#      sudo chmod 700 /opt/my_scripts/autobox.sh
-#        and copy this into it..
-# - To reboot without DOS style pop-up prompts stopping process needs config changed:
-#	  sudo vim /etc/needrestart/needrestart.conf
-#		- Outdated Daemons warning (UNcomment + change i->a):
-#			- #$nrconf{restart} = 'i';  ==>  
-#			- $nrconf{restart} = 'a';
-#		- Kernel updates notification - UNcomment this line:   
-#           - $nrconf{kernelhints} = -1;
-# - To run sudo without password storage or promptingm add whole script to sudoers so everything within it is run with sudo
-#      use command:  sudo visudo  # and add line at end:
-#                    dog ALL=(ALL) NOPASSWD: /opt/my_scripts/autobox.sh
-# - schedule once a day, run at 8:30am for now, and at reboot:  
-#          crontab -e //  30 8 * * * sudo /opt/my_scripts/autobox.sh
-#                         @reboot sudo /opt/my_scripts/autobox.sh
+
+#========
+# HISTORY
+#========
+# v0.1a - beta testig triggers
+# v0.1b - pilot running iagon (with service) maintaining box ok
+# v0.1  - no service capability - was ok, just added message into HELP when cant stop service
+#        - check if running already and message + exit if so
+# v0.2  - changed install folder from /opt/my_scripts/ to /opt/my_scripts/autobox/
+#        - remove keys into secrets file in run folder + parse these secrets
+#        - remove pre reboot warning message as has had 100% ok for long enough to assume it wont hang for unknown reasons
+#        - clearer message headings like AUTOBOX OK etc
+
 
 #========================
 # FUNCTION - Push Message - send message based on parameters passed
@@ -71,19 +80,32 @@ f_checkupdates() {
 }
 
 
+#============
+# VARIABLES - change as needed
+max_days_without_reboot=21  # max number of days to let standard updates go without applying+updating
+secrets_file="/opt/my_scripts/autobox/secrets.txt"  # path to secrets file
+
 #===================
 # MAIN - MAIN - MAIN 
 #===================
-max_days_without_reboot=21  # max number of days to let standard updates go without applying+updating
+#secrets 
+[ ! -f ${secrets_file} ] && exit  #pointless..
+apitoken="$(cat $secrets_file | grep -v ^# | grep apitoken | awk -F\= '{ print $2}' | awk -F\# '{ print $1 }' | sed 's/ //g' | tr -d '"')"
+usrtoken="$(cat $secrets_file | grep -v ^# | grep usrtoken | awk -F\= '{ print $2}' | awk -F\# '{ print $1 }' | sed 's/ //g' | tr -d '"')"
+service="$(cat $secrets_file  | grep -v ^# | grep service  | awk -F\= '{ print $2}' | awk -F\# '{ print $1 }' | sed 's/ //g' | tr -d '"')"
+[ -z $apitoken ] && exit 
+[ -z $usrtoken ] && exit 
+[ -z $service ]  && service="ok-just-updates"
 
+#duplicated istances
 if [ "$(pidof -o %PPID -x $(basename $0))" != "" ]; then 
-   # already running!! Not good.  Maybe scheduled overlappy with reboot version.  Very unlikely, but message+exit
    pushmessage="HELP LIKELY\nNEEDED!!\n$(basename $0)\nRUNNING TWICE\nEXITED 2ND JOB\n Box: `uname -n`\n User:$(whoami)\n Date: `date`"
    f_pushmessage "${apitoken}" "${usrtoken}" "${pushmessage}"
    exit 1   
 fi
 
-f_checkupdates  #load variables with different update/patch counts
+# get update stats
+f_checkupdates
 
 # Days since last reboot: https://stackoverflow.com/questions/28353409/bash-format-uptime-to-show-days-hours-minutes
 rebooted_days_ago_q=`uptime | awk -F'( |,|:)+' '{d=h=m=0; if ($7=="min") m=$6; else {if ($7~/^day/) {d=$6;h=$8;m=$9} else {h=$6;m=$7}}} {print d+0}'`
@@ -103,7 +125,7 @@ if [ $trigger_updates_f -eq 0 ]; then
    #--------
    # OK EXIT
    #--------
-   pushmessage="Box: `uname -n`\n User:$(whoami)\n Updates:\nSecDist:${security_dist_q}\nSecStnd:${security_stnd_q}\nStndDist:${standard_dist_q}\nStndStnd:${standard_stnd_q}\nTailscale:${tailscale_upd_q}\nRebootedAgo:${rebooted_days_ago_q}\nReboot Reqd:${reboot_reqd_f}\nTrigger Updates:${trigger_updates_f}\n Date: `date`"   
+   pushmessage="AUTOBOX OK\nBox: `uname -n`\n User:$(whoami)\n Updates:\nSecDist:${security_dist_q}\nSecStnd:${security_stnd_q}\nStndDist:${standard_dist_q}\nStndStnd:${standard_stnd_q}\nTailscale:${tailscale_upd_q}\nRebootedAgo:${rebooted_days_ago_q}\nReboot Reqd:${reboot_reqd_f}\nTrigger Updates:${trigger_updates_f}\n Date: `date`"   
    f_pushmessage "${apitoken}" "${usrtoken}" "${pushmessage}"
 else
    #-----------
@@ -113,19 +135,16 @@ else
    [ $node_state_pre -eq 1 ] && sudo systemctl stop ${service} >/dev/null 2>&1 && sleep 3
    systemctl is-active --quiet ${service} && node_state_post=1 || node_state_post=0
    if [ $node_state_post -eq 0 ]; then
-      pushmessage="Box: `uname -n`\n TempMsg-PreUpdates\nIf OK after time\nDelete msg"    # TEMP MESSAGE CAN DELETE ONE DAY IF IT IS STABLE
-      f_pushmessage "${apitoken}" "${usrtoken}" "${pushmessage}"
-      # Update box
-	  sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y && sudo apt autoremove -y && sleep 2
+      # UPDATE BOX
+      sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y && sudo apt autoremove -y && sleep 2
       pushmessage="Box: `uname -n`\nUPDATED+REBOOTING\nUser:$(whoami)\n Updates:\nSecDist:${security_dist_q}\nSecStnd:${security_stnd_q}\nStndDist:${standard_dist_q}\nStndStnd:${standard_stnd_q}\nTailscale:${tailscale_upd_q}\nRebootedAgo:${rebooted_days_ago_q}\nReboot Reqd:${reboot_reqd_f}\nTrigger Updates:${trigger_updates_f}\n Date: `date`"   
       f_pushmessage "${apitoken}" "${usrtoken}" "${pushmessage}"
       sleep 5
-	  # Boom
-	  sudo reboot now
+      sudo reboot now # Boom
    else
       #----
-	  # EEK - Failed to stop service - send for help
-      pushmessage="Box: `uname -n`\nHELP ME - NODE\nUNSTOPPABLE\nService: ${service}\n\nUser:$(whoami)\n Updates:\nSecDist:${security_dist_q}\nSecStnd:${security_stnd_q}\nStndDist:${standard_dist_q}\nStndStnd:${standard_stnd_q}\nTailscale:${tailscale_upd_q}\nRebootedAgo:${rebooted_days_ago_q}\nReboot Reqd:${reboot_reqd_f}\nTrigger Updates:${trigger_updates_f}\n Date: `date`"   
+      # EEK - Failed to stop service - send for help
+      pushmessage="HELP ME - SERVICE\nUNSTOPPABLE\nBox: `uname -n`\nService: ${service}\n\nUser:$(whoami)\n Updates:\nSecDist:${security_dist_q}\nSecStnd:${security_stnd_q}\nStndDist:${standard_dist_q}\nStndStnd:${standard_stnd_q}\nTailscale:${tailscale_upd_q}\nRebootedAgo:${rebooted_days_ago_q}\nReboot Reqd:${reboot_reqd_f}\nTrigger Updates:${trigger_updates_f}\n Date: `date`"   
       f_pushmessage "${apitoken}" "${usrtoken}" "${pushmessage}"
    fi
 fi
