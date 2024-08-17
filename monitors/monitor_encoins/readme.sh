54a231073444ab8833875a552b7ab5094676af67
bnchk <108313235+bnchk@users.noreply.github.com>
2024-08-17 19:52:05 +1000

    minor

--- monitors/monitor_encoins/readme.sh
+++ monitors/monitor_encoins/readme.sh
@@ -1,3 +1,3 @@
 ## MONITOR ENCOINS light server
-Script is currently running, but automated update broke with last release as the structure of implementation changed.
-Needs updates to structure in current templated form
+* Script is currently running, but automated update broke with last release (variables/structure of implementation changed)
+* Needs updates to bring old structure into current uauto templated form and lose secrets file
