378a371e270a75f05a48d161596e855048917f2c
bnchk <108313235+bnchk@users.noreply.github.com>
2024-08-17 19:40:13 +1000

    init

---
+++ monitors/monitor_encoins/readme.sh
@@ -0,0 +1,3 @@
+## MONITOR ENCOINS light server
+Script is currently running, but automated update broke with last release as the structure of implementation changed.
+Needs updates to structure in current templated form
